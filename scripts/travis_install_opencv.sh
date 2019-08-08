b4de5ced4fc2b28023bfc889cd15a814aca44c09
Jack <joh509@york.ac.uk>
2019-08-08 21:04:27 +0100

    Removed whitespace after line breaks in travis_install_opencv.sh

--- scripts/travis_install_opencv.sh
+++ scripts/travis_install_opencv.sh
@@ -3,7 +3,7 @@
 # A script for installing OpenCV from source on travis build machine instance
 
 # Install OpenCV dependencies
-sudo apt-get install -y build-essential libgtk2.0-dev pkg-config libavcodec-dev \ 
+sudo apt-get install -y build-essential libgtk2.0-dev pkg-config libavcodec-dev \
                         libavformat-dev libswscale-dev
 
 # Clone OpenCV GitHub repository
@@ -15,7 +15,7 @@ mkdir build
 cd build
 
 # Build project and install to machine
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local  \ 
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local  \
          -DBUILD_DOCS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF  \
          -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF
 
